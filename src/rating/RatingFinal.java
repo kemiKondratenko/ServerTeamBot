74d72aae5bdcc0c260fd6beeb778bb4232dc0f8f
Eugene <kemi.kondratenko@gmail.com>
2015-12-04 01:53:18 +0200

    Rate final

---
+++ src/rating/RatingFinal.java
@@ -0,0 +1,13 @@
+package rating;
+
+import com.checkers.domain.vo.Field;
+
+/**
+ * Created by Eugene on 04.12.2015.
+ */
+public class RatingFinal implements Rater{
+    @Override
+    public int rate(Field field) {
+        return 0;
+    }
+}
