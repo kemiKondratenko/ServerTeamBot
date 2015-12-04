c982a340fb5e8c42efd3292a1c0094e5fc8b029f
Vasyl Kutsyk <vasya.kutsyk@gmail.com>
2015-12-04 02:03:49 +0200

    MinMax finished

--- src/rating/FieldTree.java
+++ src/rating/FieldTree.java
@@ -28,12 +28,35 @@ public class FieldTree {
     }
 
     public Field getBestField() {
+        HashMap<FieldNode, Integer> min_3;
+        HashMap<FieldNode, Integer> max_2 = new HashMap<FieldNode, Integer>();
         for (FieldNode n : nodes) {
+            min_3 = new HashMap<FieldNode, Integer>();
+
             for (FieldNode n1 : n.getOponentsFields()) {
+                int m2 = 12;
                 for (FieldNode n2 : n1.getOponentsFields()) {
+                    if(m2 > n2.getRaiting())
+                        m2 = n2.getRaiting();
                 }
+                min_3.put(n1, m2);
+            }
+
+            int m1 = -12;
+            for(FieldNode node: min_3.keySet()){
+                if(m1 < node.getRaiting())
+                    m1 = node.getRaiting();
+            }
+            max_2.put(n, m1);
+        }
+        int m = 12;
+        FieldNode result = null;
+        for(FieldNode node: max_2.keySet()){
+            if(m > node.getRaiting()) {
+                m = node.getRaiting();
+                result = node;
             }
         }
-        return null;
+        return result.getRoot();
     }
 }
